b4cc12cf7299f999e3c8823644aecdc82f30e5e6
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-05-24 10:42:33 +0100

    third strike

--- src/main/java/org/alec/game/Game.java
+++ src/main/java/org/alec/game/Game.java
@@ -1,8 +1,8 @@
 package org.alec.game;
 
 public class Game {
-     public static void main(String args...) {
-        FizzBuzz fizzBuzz = new Fizzbuzz();
+     public static void main(String ... args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
         int i = 0;
         do {
             System.out.println(fizzBuzz.play());
